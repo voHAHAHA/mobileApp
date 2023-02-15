8fbe17f2162110dde8ced430ff5083d0ed7d19d4
voHAHAHA <sv.vov4ik@gmail.com>
2023-02-15 20:18:04 +0600

    'update'

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,10 +10,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText TextPerson;
-    private TextView logo;
+    private TextView logoSecond;
     private Button mainBtn;
 
     @Override
@@ -21,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextPerson = findViewById(R.id.TextPerson);
-        logo = findViewById(R.id.logo);
+        logoSecond = findViewById(R.id.logoSecond);
         mainBtn = findViewById(R.id.mainBtn);
 
         mainBtn.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +38,66 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (TextPerson.getText().toString().trim().equals("")) {
                     Toast.makeText(MainActivity.this, R.string.noInputUser, Toast.LENGTH_LONG).show();
+                } else {
+                    String city = TextPerson.getText().toString();
+                    String key = "1a00e2e6616c8cafdce9058e4f0f772c";
+                    String url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + key;
+
+                    new GetURLData().execute(url);
                 }
             }
         });
+    }
+    private class GetURLData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            logoSecond.setText("Waiting...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            logoSecond.setText(result);
+        }
     }
 }
